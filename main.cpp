2685b61c0d9b4c9b53c80f1d150313462aef34fb
Vahe Barseghyan <vahebarseghyan9999@gmail.com>
2024-11-11 13:08:55 +0400

    use SharedPtr class

--- main.cpp
+++ main.cpp
@@ -2,7 +2,21 @@
 
 int main()
 {
-    SharedPtr sharedPtr;
+    SharedPtr<int> ptr1(new int(5));
+    std::cout << "ptr1 count: " << ptr1.count() << std::endl;
+    std::cout << "ptr1 value: " << *ptr1 << std::endl;
+
+    {
+        SharedPtr<int> ptr2 = ptr1;
+        std::cout << "Ptr1 count after copy: " << ptr1.count() << std::endl;
+
+        SharedPtr<int> ptr3 = ptr1;
+        std::cout << "Ptr1 count after copy: " << ptr1.count() << std::endl;
+    }
+
+    ptr1.reset(new int(12));
+
+    std::cout << "Ptr1 count after ptr2 goes out of scope: " << ptr1.count() << std::endl;
 
     return 0;
 }
