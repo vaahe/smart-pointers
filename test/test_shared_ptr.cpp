2e3eb5bf3ba0c3e8572fdf1c31a085a4efeeb863
Vahe Barseghyan <vahebarseghyan9999@gmail.com>
2024-11-11 18:24:26 +0400

    Add basic initialization test for SharedPtr

---
+++ test/test_shared_ptr.cpp
@@ -0,0 +1,9 @@
+#include "shared_ptr/shared_ptr.h"
+#include <gtest/gtest.h>
+
+TEST(SharedPtrTest, BasicInitialization)
+{
+    SharedPtr<int> ptr(new int(5));
+    EXPECT_EQ(*ptr, 5);
+    EXPECT_EQ(ptr.count(), 1);
+}
